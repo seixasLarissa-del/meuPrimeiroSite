aeb9c24c8c6521af99218d1485a114ac94aa4d6d
Larissa Seixas <seixas.larissa@escola.pr.gov.br>
2026-08-06 12:15:58 -0300

    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+//botão curtida
+
+document.addEventListener("DOMContentLoaded",() => {
+    const likeBtn = document.querySelector(".left-actions .action-btn:fist-child");
+    if (condition!likeBtn) return;
+    const likeSvg {
+        
+    }
+} )
